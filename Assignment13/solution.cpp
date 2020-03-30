0293452db62d465574030c602429eacc6c5bf6db
Cuda Chen <clh960524@gmail.com>
2020-03-30 22:16:01 +0800

    add solutions of Assignment 13
    Add solutions of Assignment 13. Still wondering why using global
    variables lead to AC.

---
+++ Assignment13/solution.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+int n, a, c, f, l = 1;
+long long s, m;
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    cin >> n >> a;
+    m = a;
+    if(a >= 0)
+    {
+        s = a;
+    }
+    else
+    {
+        c = 1;
+    }
+
+    for(int i = 2; i <= n; i++)
+    {
+        cin >> a;
+        s += a;
+        if(s > m) 
+        {
+            m = s;
+            f = c;
+            l = i;
+        }
+        if(s < 0)
+        {
+            s = 0;
+            c = i;
+        }
+    }
+
+    cout << f << ' ' << l;
+
+    return 0;
+}
